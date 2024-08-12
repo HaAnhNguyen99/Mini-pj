909594917592fc72781969490f8ee447ddaa8752
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-08-12 11:36:37 +0700

    update: create pages folder

--- Frontend/components/nav/nav.js
+++ Frontend/components/nav/nav.js
@@ -1,14 +1,11 @@
 function updatePlaceholder() {
-  const inputElement = document.getElementById('search');
+  const inputElement = document.querySelector('.search input#search');
   if (window.innerWidth <= 600) {
     inputElement.placeholder = 'Tìm kiếm khoá học';
   } else {
     inputElement.placeholder = 'Tìm kiếm khoá học, bài viết, video, ...';
   }
 }
-
-// Gọi hàm khi tải trang
-updatePlaceholder();
 
 // Gọi hàm khi thay đổi kích thước màn hình
 window.addEventListener('resize', updatePlaceholder);
