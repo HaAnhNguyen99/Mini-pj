1be357317de427a9266aeedee7a02e6b4d413d1c
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-08-14 03:31:30 +0700

    Revert "Reapply "create api get all course return home page""
    This reverts commit 4216ed3bc6e3a7709ae5e081863156fff5035403.

--- src/main/java/vn/edu/likelion/service/CourseInterface.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.likelion.service;
-
-import vn.edu.likelion.model.course.CourseReturnHomePageResponse;
-
-import java.util.List;
-
-public interface CourseInterface {
-    List<CourseReturnHomePageResponse> listCourseReturnHomePage();
-}
