64ece4fba5914c098eee8838d54745423070ba4f
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-08-22 22:22:46 +0700

    Update: Learning.html

--- Frontend/services/renderChapter.js
+++ Frontend/services/renderChapter.js
@@ -33,13 +33,20 @@ function renderChapter(container, chapters) {
               .map(
                 (lesson, index) => `
                 <div class="panel-item" id="${lesson.id}">
-                  <div>
-                    <img src="../../assets/icons/play.svg" alt="" />
-                    <span>${index + 1}. ${lesson.lesson_title}</span>
+                  <div class="lesson-content">
+                    <div>
+                      <img src="../../assets/icons/play.svg" alt="" />
+                      <span>${index + 1}. ${lesson.lesson_title}</span>
+                    </div>
+                    <p>
+                      <span>${lesson.duration}</span>
+                    </p>
                   </div>
-                  <p>
-                    <span>${lesson.duration}</span>
-                  </p>
+                 ${
+                   lesson.is_done
+                     ? `<img class="done-icon" src="../../assets/icons/ok.svg" alt="" />`
+                     : ''
+                 }
                 </div>`
               )
               .join('')}
