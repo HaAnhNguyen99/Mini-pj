fbebc6e9420bff34d5c00b4ad18c1afab8ca10a9
NguyenHaAnh <haanhnguyen1221@gmail.com>
2024-08-27 21:05:15 +0700

    Delete deploy branch

--- docs/pages/components/loader/loader.js
+++
@@ -1,9 +0,0 @@
-export function showLoader() {
-  const loader = document.querySelector('#loader');
-  loader.style.display = 'block';
-}
-
-export function hideLoader() {
-  const loader = document.querySelector('#loader');
-  loader.style.display = 'none';
-}
--- docs/services/env.js
+++
@@ -1,6 +0,0 @@
-const path = 'http://127.0.0.1:3000/Frontend/';
-// const API = 'https://66b83ef23ce57325ac76b541.mockapi.io/courses/';
-
-const url = window.location.href;
-const parsedUrl = new URL(url);
-const baseUrl = `${parsedUrl.origin}/`;
